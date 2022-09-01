b562a6dc5d01aabbf2d2afc32ad28c1cb60f5622
Muzhi Zhang <zmz0072000@outlook.com>
2022-08-31 21:10:21 -0700

    Vizhub starting code

---
+++ A2a-Introduction-Vizhub/bundle.js
@@ -0,0 +1,14 @@
+(function (d3) {
+	'use strict';
+
+	// This is an example of an ES6 module.
+	const message = 'Hello VizHub!';
+
+	// You can import API functions like this from D3.js.
+
+	// This line uses D3 to set the text of the message div.
+	d3.select('#message').text(message);
+
+}(d3));
+
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzIjpbIm15TWVzc2FnZS5qcyIsImluZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIFRoaXMgaXMgYW4gZXhhbXBsZSBvZiBhbiBFUzYgbW9kdWxlLlxuZXhwb3J0IGNvbnN0IG1lc3NhZ2UgPSAnSGVsbG8gVml6SHViISc7XG4iLCIvLyBZb3UgY2FuIGltcG9ydCBBUEkgZnVuY3Rpb25zIGxpa2UgdGhpcyBmcm9tIEQzLmpzLlxuaW1wb3J0IHsgc2VsZWN0IH0gZnJvbSAnZDMnO1xuXG4vLyBZb3UgY2FuIGltcG9ydCBsb2NhbCBFUzYgbW9kdWxlcyBsaWtlIHRoaXMuIFNlZSBtZXNzYWdlLmpzIVxuaW1wb3J0IHsgbWVzc2FnZSB9IGZyb20gJy4vbXlNZXNzYWdlJztcblxuLy8gVGhpcyBsaW5lIHVzZXMgRDMgdG8gc2V0IHRoZSB0ZXh0IG9mIHRoZSBtZXNzYWdlIGRpdi5cbnNlbGVjdCgnI21lc3NhZ2UnKS50ZXh0KG1lc3NhZ2UpO1xuIl0sIm5hbWVzIjpbInNlbGVjdCJdLCJtYXBwaW5ncyI6Ijs7O0NBQUE7Q0FDTyxNQUFNLE9BQU8sR0FBRyxlQUFlOztDQ0R0QztBQUtBO0NBQ0E7QUFDQUEsVUFBTSxDQUFDLFVBQVUsQ0FBQyxDQUFDLElBQUksQ0FBQyxPQUFPLENBQUM7Ozs7In0=
